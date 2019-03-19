78e8f4791799f16b79c3204d87368c8fbfdc376a
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-19 22:33:50 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist1 = {artistName: 'songTitle'}; 
+var playlist = {artistName: 'songTitle'}; 
 
 function updatePlaylist (playlist, artistName, songTitle){
   return Object.assign ({}, playlist, { [artistName]: songTitle}) 
