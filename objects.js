dbb7988b1420234d2c795a428e23da6cbc24002a
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-19 22:41:34 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -13,3 +13,8 @@ function  updatePlaylist (playlist, artistName, songTitle){
   playlist['artistName'] = 'songTitle';
   return  playlist;
 }
+
+function  removeFromPlaylist(artistName, playlist)  {
+  delete playlist['artistName'];
+  return  playlist;
+}
